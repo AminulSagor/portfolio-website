a5f749d02d6f5ab643633ca9070ad6764cff973f
JINNAT00 <jinnatara954@gmail.com>
2024-12-28 01:05:11 +0600

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+exports.signup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        // Check if user exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create new user
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({ message: "User created successfully" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
